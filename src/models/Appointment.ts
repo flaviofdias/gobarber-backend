16e107dae2ce9cae2cc91931d0f99493bd8f9545
Flavio Dias <flaviodias.info@gmail.com>
2020-06-23 21:48:41 -0300

    Add Decorator Database

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,4 +1,3 @@
-import { uuid } from 'uuidv4';
 import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 
 // Declaração nome tabela
@@ -12,13 +11,6 @@ class Appointment {
 
   @Column('timestamp with time zone')
   date: Date;
-
-  // Omit, permite excluir variáveis
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
