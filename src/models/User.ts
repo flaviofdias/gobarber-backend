9a3fa29277e1bbb0126de18d09d78dca0b89c44b
Flavio Dias <flaviodias.info@gmail.com>
2020-06-24 00:47:14 -0300

    Settings Migration

--- src/models/User.ts
+++ src/models/User.ts
@@ -7,16 +7,19 @@ import {
 } from 'typeorm';
 
 // Declaração nome tabela
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Column()
-  provider: string;
+  name: string;
 
-  @Column('timestamp with time zone')
-  date: Date;
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
 
   @CreateDateColumn()
   created_at: Date;
@@ -25,4 +28,4 @@ class Appointment {
   updated_at: Date;
 }
 
-export default Appointment;
+export default User;
