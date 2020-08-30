d606845a033bac228c8d796dbc7f64e92f33d4be
Flavio Dias <flaviodias.info@gmail.com>
2020-08-30 07:33:09 -0300

    Add Validation - Celebrate

--- src/modules/users/infra/http/routes/profile.routes.ts
+++ src/modules/users/infra/http/routes/profile.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate'; // Validação de dados
 
 import ProfileController from '@modules/users/infra/http/controllers/ProfileController';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -9,6 +10,18 @@ const profileController = new ProfileController();
 profileRouter.use(ensureAuthenticated); // Acessível se estiver logado
 
 profileRouter.get('/', profileController.show);
-profileRouter.put('/', profileController.update);
+profileRouter.put(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required,
+      old_password: Joi.string(),
+      password: Joi.string().required(), // Pode melhorar, tornando obrigatório quando o campo old_password existir
+      password_confirmation: Joi.string().valid(Joi.ref('password')), // Pode melhorar, tornando obrigatório quando o campo old_password existir
+    },
+  }),
+  profileController.update,
+);
 
 export default profileRouter;
