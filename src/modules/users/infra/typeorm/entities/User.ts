101b060521c197655d9b0239445b70da5b686bce
Flavio Dias <flaviodias.info@gmail.com>
2020-09-02 01:03:00 -0300

    Upload files from Amazon S3

--- src/modules/users/infra/typeorm/entities/User.ts
+++ src/modules/users/infra/typeorm/entities/User.ts
@@ -6,7 +6,9 @@ import {
   UpdateDateColumn,
 } from 'typeorm';
 
+import uploadConfig from '@config/upload';
 import { Exclude, Expose } from 'class-transformer';
+import { is } from 'date-fns/locale';
 
 // Declaração nome tabela
 @Entity('users')
@@ -35,10 +37,18 @@ class User {
 
   @Expose({ name: 'avatar_url' })
   getAvatarUrl(): string | null {
-    // String ou Nulo
-    return this.avatar
-      ? `${process.env.APP_API_URL}/files/${this.avatar}`
-      : null;
+    if (!this.avatar) {
+      return null;
+    }
+
+    switch (uploadConfig.driver) {
+      case 'disk':
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case 's3':
+        return `https://${uploadConfig.config.aws.bucket}.s3.amazonaws.com/${this.avatar}`;
+      default:
+        return null;
+    }
   }
 }
 
